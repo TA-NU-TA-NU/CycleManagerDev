c9259a2045b19506c13d2c970479ccfc335f8ac4
tanu tanu <lydian.eca@gmail.com>
2022-02-12 12:20:01 +0900

    SpringInitiarizrのzip取り込み -> image更新

---
+++ java/spring_app/cyclemgr/src/main/java/com/tanutanu/cyclemgr/CyclemgrApplication.java
@@ -0,0 +1,13 @@
+package com.tanutanu.cyclemgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclemgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclemgrApplication.class, args);
+	}
+
+}
---
+++ java/spring_app/cyclemgr/src/test/java/com/tanutanu/cyclemgr/CyclemgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanutanu.cyclemgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclemgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
